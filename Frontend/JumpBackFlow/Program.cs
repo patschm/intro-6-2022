293131ba44daef8d417e184f66a4aaa8e3a6ea1e
Patrick <pschmidt@pasit.nl>
2022-06-28 10:21:06 +0200

    tweede

---
+++ Frontend/JumpBackFlow/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace JumpBackFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            // Gebruik deze voor bijv user invoer te valideren en hervragen.
+            // Wordt 1 of meer keer uitgevoerd.
+            do
+            {
+                Console.WriteLine("Geef een getal groter dan 10");
+                counter = int.Parse(Console.ReadLine());
+            }
+            while (counter < 10);
+
+            // Gebruik je om data uit te lezen (tabel, file)
+            // Wordt 0 of vaker uitgevoerd
+            while (counter < 10)
+            {
+                Console.WriteLine("Counter = " + counter);
+                counter++;
+            }
+
+            //int teller = 0;
+            // Je weet hoe vaak je iets moet herhalen
+            for (int teller = 0; teller < 10 ;teller++)
+            {
+                Console.WriteLine("Loop " + teller);
+            }
+        }
+    }
+}
